713c78cff4df9ce473d0ec32140cddceb397c3e3
Dmitry Yurusov <dyurusov@gmail.com>
2021-11-15 15:24:49 +0300

    fix db init

--- config/container.php
+++ config/container.php
@@ -14,7 +14,7 @@ $definitions = [
     App\Repositories\TaskRepositoryInterface::class => DI\create(App\Repositories\TaskRepository::class)
         ->constructor(DI\get(PDO::class)),
 
-    PDO::class => DI\factory(function ($dsn = null, $user = null, $pass = null, $options = null): PDO {
+    PDO::class => DI\factory(function ($dsn = null, $user = null, $pass = null, $options = null) use ($config): PDO {
         $dbh = new PDO($dsn, $user, $pass, $options);
         $sqlInit = $config['db']['scripts']['init'] ?? null;
         if ($sqlInit) {
